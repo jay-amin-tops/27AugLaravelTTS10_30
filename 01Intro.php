2ab02b8b0fe99a10b44adcaa62b039b9ef322162
jay-amin-tops <jay.amin.tops@gmail.com>
2022-08-27 11:14:38 +0530

    first commit

---
+++ 01Intro.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+
+<body>
+
+    <p>What is website</p>
+    <p>What is programming Lang</p>
+    <p>What is Scripting Lang</p>
+    <p>What is SEO</p>
+    <p>What is WWW</p>
+    <p>What is Domnain</p>
+    <p>What is Clilent </p>
+    <p>What is Server</p>
+    <p>What is Hosting</p>
+    <p>What is Application Server</p>
+    <p>What is DataBase Server</p>
+    <p>What is editor</p>
+    <p>What is xampp => package cross plateform Apache MariaDB PHP perl</p>
+
+</body>
+
+</html>
